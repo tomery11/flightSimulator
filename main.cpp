7b87cbc20a4568fb516cfdc3e30ad4b57e5c73da
tairch <tairch@gmail.com>
2018-12-17 03:57:53 +0200

    fixed a bug: one parameter is 2 in argc

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@
 using namespace std;
 
 
-void lexer(string input, vector<string> inputVec){
+void lexer(const string *input, vector<string> inputVec){
 
     cout << "lexer" << endl;
     //todo add more whitespaces to token?
     string token = " \t\n";
-    std::istringstream iss(input);
+    std::istringstream iss(*input);
     //if not in while loop or in if condition, delete the vector contents
     if (!inputVec.empty() && (inputVec.at(0) != "while" || inputVec.at(0) != "if")) {
         inputVec.clear();
@@ -51,9 +51,9 @@ void parser(const vector <string> *inputVec, const map<string,Command*> *mapComm
 //get a file as argument or no arguments for getting lines from the user.
 int main(int argc, char *argv[]) {
     //one argument to get the script from a file
-    if (argc == 1) { //todo
+    if (argc == 2) { //todo
 
-    } else if (argc == 0) { //no argument for per line parsing
+    } else if (argc == 1) { //no argument for per line parsing
         std::string input;
         vector<string> inputVec;
         //create map string Command
@@ -66,7 +66,7 @@ int main(int argc, char *argv[]) {
 
         do {
             getline(std::cin, input);
-            lexer(input, inputVec);
+            lexer(&input, inputVec);
             parser(&inputVec, &commandMap);
 
         } while (input != "print \"done\"");
