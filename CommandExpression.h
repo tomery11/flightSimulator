177d1b7e89d5d70c84468b96818f6b47370c48f6
Tomer Yona <tomeryona92@gmail.com>
2018-12-21 19:25:13 +0200

    Shunting yard Algorithm-updated, need to debug

---
+++ CommandExpression.h
@@ -0,0 +1,18 @@
+//
+// Created by Tomer Yona on 2018-12-21.
+//
+
+#ifndef FLIGHTSIMULATOR_COMMANDEXPRESSION_H
+#define FLIGHTSIMULATOR_COMMANDEXPRESSION_H
+
+#include "Expression.h"
+#include "Command.h"
+
+class CommandExpression : public Expression {
+    double calculate();
+
+
+};
+
+
+#endif //FLIGHTSIMULATOR_COMMANDEXPRESSION_H
