c9509705bacebad1989d36e6320d9b386d30c25c
tomery11 <tomeryona92@gmail.com>
2018-12-25 01:45:45 +0200

    Shunting yard deubugged, one case not perfect yet

--- main.cpp
+++ main.cpp
@@ -14,6 +14,15 @@
 #include "PrintCommand.h"
 #include "SleepCommand.h"
 #include "EntercCommand.h"
+//for shunting yard test
+#include "BinaryExpression.h"
+#include "Expression.h"
+#include "Number.h"
+#include "Plus.h"
+#include "Minus.h"
+#include "Multiply.h"
+#include "Divide.h"
+#include "ShuntingYard.h"
 ///home/t/CLionProjects/flightSimulator/script
 using namespace std;
 
@@ -75,6 +84,14 @@ void parser(vector <string> *inputVec, const map<string,Command*> *mapCommand, S
 
 //get a file as argument or no arguments for getting lines from the user.
 int main(int argc, char *argv[]) {
+
+    ShuntingYard sh_test;
+    string test1="5-12";
+    double test1_ans= sh_test.evaluate(test1);
+    cout<<test1_ans<<endl;
+
+
+
     std::string input;
     vector<string> inputVec;
     //create symbol map - variable name and it's bind
