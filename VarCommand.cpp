cffd7d6420cd37a835b5451745d18b3c8a9d0bf3
tomery11 <tomeryona92@gmail.com>
2018-12-26 14:10:40 +0200

    checks that var has valid name

--- VarCommand.cpp
+++ VarCommand.cpp
@@ -5,10 +5,18 @@
 #include "VarCommand.h"
 
 void VarCommand::doCommand(std::vector<string> *inputVec) {
-    //todo input validation:cant start with a number, no saved words,
-    // only english chars, numbers and underscore.
+
     this->name = inputVec->at(1);
     //get the subset with no ""
+    bool varValid= false;
+    if(name[0]>47 && name[0] < 58)
+        throw "bad input var name";
+    for(int i = 0; i<name.length(); i++){
+        if((name[i]<=47 && name[i]>=58) && ( name[i]<=64 && name[i]>=91 )||
+        ( name[i]<=96 && name[i]>=123 ) && (name[i] != '_') ){
+                throw "bad input var name";
+        }
+    }
     this->bind = inputVec->at(4).substr(1, inputVec->at(4).length() - 2);
     //add to symbols
     if (this->symbols == NULL) {
